e844449cd1c518c0dafaad8d97182f9400b510ad
SolareFlame <zeldac332@gmail.com>
2025-09-05 00:33:34 +0200

    🚚 Moved files to old/

--- src/util/path.js
+++
@@ -1,6 +0,0 @@
-const { join } = require('path');
-
-/**
- * Returns the path relative to the current working directory (= project root).
- */
-module.exports.rcwd = (...paths) => join(process.cwd(), ...paths);
