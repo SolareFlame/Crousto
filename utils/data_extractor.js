ad7698ab5025160de5698fb67b76d18ba1bea796
SolareFlame <zeldac332@gmail.com>
2025-09-06 02:52:59 +0200

    🎨 New utils to manipulate api html

---
+++ utils/data_extractor.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+
+function parseHtml(html) {
+    const decoded = html
+        .replace(/\\u003C/g, "<")
+        .replace(/\\u003E/g, ">")
+        .replace(/\\"/g, '"');
+
+    const $ = cheerio.load(decoded);
+
+    const sections = [];
+
+    $("h2").each((_, el) => {
+        const title = $(el).text().trim();
+        const content = $(el).next("p").html()
+            ?.replace(/<br\s*\/?>/gi, "\n")
+            ?.replace(/<img[^>]*alt=["']?([^"']+)["']?[^>]*>/gi, "[$1]")
+            ?.replace(/<[^>]+>/g, "")
+            ?.trim();
+
+        if (content) {
+            sections.push({ title, content });
+        }
+    });
+
+    return {sections };
+}
+
+module.exports = { parseHtml };
