d09e6b45a55617f41da01846f2fed52343bb6fe2
SolareFlame <zeldac332@gmail.com>
2025-09-08 03:14:44 +0200

    🚧 Working on follow system

---
+++ commands/follow.js
@@ -0,0 +1,63 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+const {getRestaurant} = require('../services/data/restaurantService');
+const {getMenu} = require('../services/data/menuService');
+
+const {renderMenu} = require('../services/message/menuMessage');
+const {getAllRestaurants} = require("../services/data/restaurantService");
+
+const heures = [];
+for (let h = 8; h <= 21; h++) {
+    heures.push({ name: `${h}h`, value: `${h}`});
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('follow')
+        .setDescription('Suivre le menu d\'un restaurant')
+        .addStringOption(option =>
+            option.setName('id')
+                .setDescription('ID du restaurant')
+                .setRequired(true)
+                .setAutocomplete(true)
+        )
+        .addStringOption(option =>
+            option.setName('heure')
+                .setDescription('Heure de la notification')
+                .setRequired(true)
+                .addChoices(...heures)
+        )
+        .addRoleOption(option =>
+            option.setName('role')
+                .setDescription('Rôle à mentionner lors de la notification')
+                .setRequired(false)
+        ),
+
+    async autocomplete(interaction) {
+        const restaurants = await getAllRestaurants();
+        const focused = interaction.options.getFocused();
+
+        const choices = restaurants.map(r => ({
+            name: r.title,
+            value: String(r.sourceId),
+        }));
+
+        const filtered = choices.filter(choice =>
+            choice.name.toLowerCase().includes(focused.toLowerCase())
+        );
+
+        await interaction.respond(filtered.slice(0, 25));
+    },
+
+    async execute(interaction) {
+        const restaurant_id = interaction.options.getString('id');
+        const heure = interaction.options.getString('heure');
+        const role = interaction.options.getRole('role');
+
+        const channel_id = interaction.channelId;
+        const guild_id = interaction.guildId;
+
+        //const render = TODO
+        await interaction.editReply(render);
+    },
+};
