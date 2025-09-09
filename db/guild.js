2bc1c3e9075f45df46ca4c7d6f4c32c6ed2ecf23
SolareFlame <zeldac332@gmail.com>
2025-09-09 18:12:13 +0200

    ✨ New guild service

--- db/guild.js
+++ db/guild.js
@@ -1,14 +1,22 @@
 import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
-export function addGuild(guildId, name) {
-    return prisma.guild.create({
-        data: {guildId, name},
+export function upsertGuild(guildId, name, ownerId) {
+    return prisma.guild.upsert({
+        where: {guildId},
+        update: {name, ownerId},
+        create: {guildId, name, ownerId},
     });
 }
 
-export function removeGuild(guildId) {
+export async function removeGuild(guildId) {
     return prisma.guild.delete({
+        where: {guildId},
+    });
+}
+
+export async function getGuild(guildId) {
+    return prisma.guild.findUnique({
         where: {guildId},
     });
 }
---
+++ services/data/guildService.js
@@ -0,0 +1,18 @@
+import {upsertGuild} from "../../db/guild.js";
+
+export async function addGuild(guildId, client) {
+    const guild = await client.guilds.fetch(guildId);
+    if (!guild) {
+        console.error(`GuildService: Guild with ID ${guildId} not found.`);
+        return;
+    }
+
+    const name = guild.name;
+    const ownerId = guild.ownerId;
+
+    try {
+        await upsertGuild(guildId, name, ownerId);
+    } catch (error) {
+        console.error(`GuildService: Error adding/updating guild ${name} (${guildId}):`, error);
+    }
+}
