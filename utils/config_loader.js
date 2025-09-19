4cc8dc346c8bedcf25c23efeca1447d2eada4815
SolareFlame <zeldac332@gmail.com>
2025-09-19 13:58:50 +0200

    ✨ Enhance config loading with logging

--- utils/config_loader.js
+++ utils/config_loader.js
@@ -6,12 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {typeof import('../config/config.json')} */
-export const config = loadConfig();
+export const config = loadConfig(true);
 
-function loadConfig() {
+function loadConfig(_logs = false) {
     const configPath = path.join(__dirname, '..', 'config', 'config.json');
     try {
-        return JSON.parse(fs.readFileSync(configPath, 'utf8'));
+        const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+        console.log('Config loaded successfully:', config)
+        return config;
     } catch (err) {
         console.error('ERROR loading config:', err);
         return {};
