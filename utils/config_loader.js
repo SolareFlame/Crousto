841275c4894fae84e1b99181329b71d13c3a61f1
SolareFlame <zeldac332@gmail.com>
2025-09-19 13:56:07 +0200

    ✨ Update config structure with new visuals and emojis

--- utils/config_loader.js
+++ utils/config_loader.js
@@ -1,95 +1,19 @@
 import fs from 'fs';
-
-import { fileURLToPath } from "url";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-
-/**
- * @typedef {Object} Config
- * @property {ApiUniv} api_univ
- * @property {Visuals} visuals
- * @property {Data} data
- * @property {DbCache} db_cache
- */
-
-/**
- * @typedef {Object} ApiUniv
- * @property {string} url
- * @property {ApiUnivEndpoints} endpoints
- */
-
-/**
- * @typedef {Object} ApiUnivEndpoints
- * @property {ApiEndpoint} restaurants
- * @property {ApiEndpoint} menus
- */
-
-/**
- * @typedef {Object} ApiEndpoint
- * @property {string} endpoint
- * @property {string[]} [params]
- */
-
-/**
- * @typedef {Object} Visuals
- * @property {Colors} colors
- * @property {Logos} logos
- * @property {Banners} banners
- */
-
-/**
- * @typedef {Object} Colors
- * @property {string} primary
- * @property {string} secondary
- */
-
-/**
- * @typedef {Object} Logos
- * @property {string} default
- */
-
-/**
- * @typedef {Object} Banners
- * @property {string} default
- */
-
-/**
- * @typedef {Object} Data
- * @property {string} bot_name
- * @property {string} github_url
- * @property {string} cron_subscription
- * @property {string} cron_daily_fetch
- */
-
-/**
- * @typedef {Object} DbCache
- * @property {number} menu_validity_time
- * @property {number} restaurant_validity_time
- */
-
-/** @type {Config} */
+/** @type {typeof import('../config/config.json')} */
 export const config = loadConfig();
 
-/**
- * Load configuration from config.json file.
- * @returns {Config} The configuration object.
- */
-export function loadConfig() {
-    const config_path = path.join(__dirname, '..', 'config', 'config.json');
-
-    let file_config = {};
+function loadConfig() {
+    const configPath = path.join(__dirname, '..', 'config', 'config.json');
     try {
-        const raw = fs.readFileSync(config_path, 'utf8');
-        file_config = JSON.parse(raw);
+        return JSON.parse(fs.readFileSync(configPath, 'utf8'));
     } catch (err) {
-        console.error('Utils: ERROR while loading config=', err.message);
+        console.error('ERROR loading config:', err);
+        return {};
     }
-
-    console.log("Utils: Config loaded !");
-    console.log(file_config);
-
-    return file_config;
 }
